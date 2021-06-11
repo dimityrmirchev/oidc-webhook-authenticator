243e32f9ecf329e39fd1f8bcc8a2416f76742a01
Dimitar Mirchev <dimitar.mirchev@sap.com>
2021-06-11 10:52:29 +0300

    Permissions fix

--- hack/local/run-tests.sh
+++ hack/local/run-tests.sh
@@ -13,7 +13,7 @@ fi
 if ! command -v kubebuilder &> /dev/null
 then
     curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
-    chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
+    chmod +x kubebuilder && sudo mv kubebuilder /usr/local/bin/
 fi
 
 if ! command -v setup-envtest &> /dev/null
